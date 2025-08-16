c46bf5b703cb6fe000d89008ed0adafc8e634386
Erfan Vafaei <Erfan.Vafaei1383@gmail.com>
2025-08-16 21:39:12 +0330

    first commit

---
+++ snake game.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+#-----------------------------
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPACE_SIZE = 50
+SLOWNESS = 200
+SNAKE_COLOR = "blue"
+FOOD_COLOR = "red"
+score = 0
+direction = "down"
+#-----------------------------
+window = Tk()
